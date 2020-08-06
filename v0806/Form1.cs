11863c94348746f6f2914adeb2d5404251b9f964
shinon0821 <49701829+shinon0821@users.noreply.github.com>
2020-08-06 09:31:53 +0900

    完成

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -12,9 +12,30 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx;
+        int vy;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            vx = -10;
+            vy = -10;
+            label1.Left += vx;
+            label1.Top += vy;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = 10;
+            vy = 10;
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
